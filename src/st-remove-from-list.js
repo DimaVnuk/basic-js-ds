ae79a238baab3f1da93517624cca8c20bb5913d0
DimaVnuk <71352399+DimaVnuk@users.noreply.github.com>
2021-10-15 20:47:28 +0300

    Update st-remove-from-list.js
    feat: add linked list with recursion

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -19,7 +19,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function removeKFromList( l, k ) {
+  if(value === k){
+      return next ? removeKFromList(next, k) : null;
+   } else {
+      return {
+        next : removeKFromList(next),
+        value
+   };
+ }
 }
